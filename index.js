b7a627cf5f75d491fa4c648333f3eeabade5255b
Jordan Halfman <jordanhalfman@gmail.com>
2022-04-29 19:49:19 -0700

    commit tree 25dbd409237fd10d14ba4e4be7a2a31e35d4399b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(task1 = "roller-skate") {
+    return "This Saturday, I want to " + task1 + "!";
+}
+
+function mondayWork(task2 = "go to the office") {
+    return `This Monday, I will ${task2}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(task3 = "special"){
+        return `You are ${flair}${task3}${flair}!`;
+    }
+}
